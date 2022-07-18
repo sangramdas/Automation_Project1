2348787cbd24d33a6630bb2ee5a643e7c5382c09
root <root@ip-172-31-19-184.ec2.internal>
2022-07-18 17:06:52 +0000

    Modified automation.sh for task3

--- automation.sh
+++ automation.sh
@@ -11,6 +11,9 @@ timestamp=$(date '+%d%m%Y-%H%M%S')
 
 myname="Sangram"
 
+#Define variable for File of Log Metadata
+FILE="/var/www/html/inventory.html"
+
 #Check is Apache2 is installed and Running
 c2=`dpkg --get-selections | grep apache`
 if [[ $? > 0 ]]
@@ -28,7 +31,38 @@ fi
 #Create tar compressed files of Apache2 Logs
 tar -czvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log 
 
-# Copy tar file to S3
+#Copy tar file to S3
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+#Create Inventoory file
+logfile=`ls -ltrh /tmp/*$timestamp*`
+var1=`echo $logfile | cut -f5 -d' '`
+var2=`echo $logfile | cut -f9 -d' ' | cut -f3 -d'/'`
+var3=`echo $logfile | cut -f9 -d' ' | cut -f3 -d'/' | cut   -f2 -d'-'`
+var4=`echo $logfile | cut -f9 -d' ' | cut -f3 -d'/' | cut   -f3 -d'-'`
+var5=`echo $logfile | cut -f9 -d' ' | cut -f3 -d'/' | cut -f2 -d'.'`
+if test -f "$FILE"
+then
+    echo -e "$var3-$var4\t$timestamp\t$var5\t$var1" >> $FILE
+else
+    echo -e "Log Type\tDate Created\tType\tSize" > $FILE
+    echo -e "$var3-$var4\t$timestamp\t$var5\t$var1" >> $FILE
+
+fi
+
+#Create cronjob if it does not exists
+CRON='/etc/cron.d/automation'
+if test -f "$CRON"
+then
+    echo "cron exists"
+else
+    touch $CRON
+    echo "SHELL=/bin/bash" > $CRON
+    echo "PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" >> $CRON
+    echo "0 17 * * * root /root/Automation_Project/automation.sh" >> $CRON
+fi
+
+
+
 
 
